2bb0f94574313baaa3a63e4118189ddfc5c5b46e
yaogeeg666 <3162987998@qq.com>
2019-01-12 20:24:25 +0800

    import element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app">
-        <h1>Hello world</h1>
+
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Element from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+
 Vue.config.productionTip = false
 
 new Vue({
