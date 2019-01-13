d01e46892a5df45bba3e6d5bf1d9fe9e3a33fb36
martsforever <martsforever@aliyun.com>
2019-01-13 21:00:25 +0800

    adjust home menu

--- src/page/home/home-content.vue
+++ src/page/home/home-content.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home-content">
-       <stu_detail/>
+        <stu_detail/>
         <stu_duty/>
         <stu_score/>
         <stu_vac/>
@@ -12,6 +12,7 @@
     import Stu_duty from "../student/stu-duty";
     import Stu_score from "../student/stu_score";
     import Stu_vac from "../student/stu_vac";
+
     export default {
         name: "home-content",
         components: {Stu_vac, Stu_score, Stu_duty, Stu_detail}
